7215aa0ee347c565b82af981b4e5c1dd434fed9b
kaiquecoqueiro <kaiquecoqueiro@hotmail.com>
2021-05-08 22:53:34 -0300

    Oops

--- DataStructsAndAlgo/firstRecurringCharacter.js
+++ DataStructsAndAlgo/firstRecurringCharacter.js
@@ -20,7 +20,7 @@ function firstRecurringCharacter(input) {
       return undefined
 }
 
-function firstRecurringCharacter2(input) {
+function firstRecurringCharacter2(input) { 
     let map = {}
 
     for(let i = 0; i < input.length; i++) {
